dbce4cfe9d168e6fbc6952cdc594856838da622a
Samuel Marks <samuelmarks@gmail.com>
2017-10-05 13:39:46 +1100

    Fixed ordering of updates

--- update_all.bash
+++ update_all.bash
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Might as well...
 bash "$DIR/kill_all.bash"
 
+bash "$DIR/update_scripts.bash"
 bash "$DIR/update_web_frontend.bash"
 bash "$DIR/update_api.bash"
-bash "$DIR/update_scripts.bash"
